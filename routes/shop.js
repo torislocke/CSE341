9c8ef4275d8e1f08319ec50cae1eaf721027747c
tslocke <tori.bosler@hotmail.com>
2021-04-23 13:40:34 -0700

    Add products json file, models and updated routes

--- routes/shop.js
+++ routes/shop.js
@@ -2,16 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-	const products = adminData.products;
-	res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
-	// no longer need below after adding ejs template engine
-	// res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
